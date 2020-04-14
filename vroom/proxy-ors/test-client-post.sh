8b1baccfee4224f248f62858d1b8f341a468e628
stefanocudini <stefano.cudini@gmail.com>
2020-04-14 22:36:25 +0200

    vroom proxy

--- vroom/proxy-ors/test-client-post.sh
+++ vroom/proxy-ors/test-client-post.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 #
 curl --data "param1=value1&param2=value2" http://localhost:8000/ciao
+
+
+#curl --header "Content-Type:application/json" --data '{"vehicles":[{"id":0,"start":[2.3526,48.8604],"end":[2.3526,48.8604]}],"jobs":[{"id":0,"location":[2.3691,48.8532]},{"id":1,"location":[2.2911,48.8566]}],"options":{"g":true}}' http://localhost:3000
