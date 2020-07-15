1f1b73badeacf29740bb2e6d15897afdc5dadad0
stefanocudini <stefano.cudini@gmail.com>
2020-07-15 16:49:01 +0200

    fix access.log

--- vroom/docker-entrypoint.sh
+++ vroom/docker-entrypoint.sh
@@ -3,11 +3,11 @@
 
 # copy the config.yml to the host if it doesn't exist yet
 # or copy it to the source if does exist
-if test -f /conf/config.yml; then
-  cp /conf/config.yml /vroom-express/config.yml
-else
-  cp /vroom-express/config.yml /conf/config.yml
-fi
+#if test -f /conf/config.yml; then
+#  cp /conf/config.yml /vroom-express/config.yml
+#else
+#  cp /vroom-express/config.yml /conf/config.yml
+#fi
 
 # Create access.log if it doesn't exist
 #if ! test -f /conf/access.log; then
